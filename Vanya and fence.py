e32e1e05e7cc9db261d59efb81a02252846dbe7f
Abdurahman <Absr889@gmail.com>
2025-12-04 16:02:22 +0300

    Fix indentation and formatting in Vanya and fence.py

--- Vanya and fence.py
+++ Vanya and fence.py
@@ -1,13 +1,10 @@
-# Read input values
 n, h = map(int, input().split())
 a = list(map(int, input().split()))
-
-# Calculate the required width
 width = 0
 for height in a:
     if height > h:
-        width += 2  # Needs to bend
+        width += 2  
     else:
-        width += 1  # Walks normally
+        width += 1  
       
 print(width)
